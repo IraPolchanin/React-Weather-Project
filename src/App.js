ccc045ff7fd6d88f477f0357a85c6dabf53a8cf2
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-09-27 12:51:45 +0300

    added Git url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ export default function App() {
   return (
     <div className="App">
       Weather
-    </div>
+      <footer>
+        This project was coded by Iryna Polchaninova and is
+        <a href="https://github.com/IraPolchanin/React-Weather-Project"> open-sourced on GitHub</a>
+      </footer>
+    </div >
   );
 }
 
