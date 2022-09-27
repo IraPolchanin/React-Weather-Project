e73874bdbebee84921a2a651795c7a5fb8f49b7e
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-09-27 14:42:26 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather
-      <footer>
-        This project was coded by Iryna Polchaninova and is
-        <a href="https://github.com/IraPolchanin/React-Weather-Project"> open-sourced on GitHub</a>
-      </footer>
+      <div className='container'>
+        Weather
+        <footer>
+          This project was coded by Iryna Polchaninova and is
+          <a href="https://github.com/IraPolchanin/React-Weather-Project"> open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div >
   );
 }
