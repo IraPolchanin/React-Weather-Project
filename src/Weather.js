a4d97037ca40961e6a6fce2e238c4eb667b0d888
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-09-29 11:54:12 +0300

    improved css

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Weather.css';
+
 export default function Weather() {
     return (
         <div className="Weather">
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input class="form-control" type="search" placeholder="Enter a city.." />
+                        <input class="form-control" type="search" placeholder="Enter a city.." autoFocus="on" />
                     </div>
                     <div className="col-3">
                         <input type="submit" value="Search" className="btn btn-primary w-100" />
@@ -20,10 +21,15 @@ export default function Weather() {
                 <li>Tuesday 12:00</li>
                 <li>Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy" />
-                    15˚C
+                    <div className="d-flex">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy" className="float-left" />
+                        <div className="float-left">
+                            <span className="temperature">15</span>
+                            <span className="unit">˚C</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
