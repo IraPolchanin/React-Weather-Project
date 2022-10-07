c49ad1165a7c1bf06c36b95efeb48070e36ee8c0
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-10-07 11:41:47 +0300

    added WeatherForecast component

---
+++ src/WeatherForecast.js
@@ -0,0 +1,24 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                    <div className="WeatherForecastDay">Thu</div>
+                    <WeatherIcon code="01d" size={36} />
+                    <div className="WeatherForecastTemperature">
+                        <span className="WeatherForecastTemperatureMax">
+                            19˚
+                        </span>
+                        <span className="WeatherForecastTemperatureMin">
+                            10˚
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
